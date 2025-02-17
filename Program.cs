9cc77dae8323989bd7548362cf06d7305bb1a016
demir <demir@ELIFNUR>
2025-02-17 14:20:55 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Boyunuzu metre cinsinden giriniz: ");
+        double boy = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Kilonuzu kilogram cinsinden giriniz: ");
+        double kilo = Convert.ToDouble(Console.ReadLine());
+
+        double bmi = kilo / (boy * boy);
+
+        Console.WriteLine($"Beden Kitle İndeksiniz: {bmi:F2}");
+
+
+        if (bmi < 18.5)
+        {
+            Console.WriteLine("Kategori: Zayıf");
+        }
+        else if (bmi >= 18.5 && bmi < 24.9)
+        {
+            Console.WriteLine("Kategori: Normal Kilolu");
+        }
+        else if (bmi >= 25 && bmi < 29.9)
+        {
+            Console.WriteLine("Kategori: Kilolu");
+        }
+        else 
+        {
+            Console.WriteLine("Kategori: Obez");
+        }
+       
+    }
+}
